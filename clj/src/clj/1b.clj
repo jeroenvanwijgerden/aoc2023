c362e8053a2587392c13cadb1a3bfbb76f1e8f14
Jeroen van Wijgerden <jeroen.van.w@hotmail.com>
2023-12-02 11:58:24 +0100

    day 1 part 2

--- clj/src/clj/1b.clj
+++ clj/src/clj/1b.clj
@@ -1,9 +1,30 @@
 (ns clj.1b
-  (:require [clj.input :as input]))
+  (:require [clj.input :as input]
+            [clojure.string :as s]))
+
+(def word->digit
+  {"one"   "1"
+   "two"   "2"
+   "three" "3"
+   "four"  "4"
+   "five"  "5"
+   "six"   "6"
+   "seven" "7"
+   "eight" "8"
+   "nine"  "9"})
+
+(def word-or-digit-at-start (re-pattern (str "^("
+                                             (s/join "|" (concat (keys word->digit)
+                                                                 (vals word->digit)))
+                                             ")")))
 
 (defn calibration-value [line]
   (->> line
-       (filter #(Character/isDigit %))
+       (iterate rest)
+       (take-while seq)
+       (map #(apply str %))
+       (keep #(first (re-find word-or-digit-at-start %)))
+       (map #(get word->digit % %))
        ((juxt first last))
        (apply str)
        (Integer/parseInt)))
